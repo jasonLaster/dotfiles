6809466874669d21d32383360b391fdf829efb93
Jason Laster <jason.laster.11@gmail.com>
2016-02-17 22:27:26 -0500

    add functions

--- functions.zsh
+++ functions.zsh
@@ -1,18 +1,32 @@
-function edit_modified() {
+function edit-modified() {
     vim -p `git status --porcelain | awk '{print $2}'`
 }
- 
-function edit_conflicts() {
+
+function edit-conflicts() {
     vim -p `git diff --name-only --diff-filter=U`
 }
- 
-function edit_commit {
+
+function edit-commit {
     if [ -z "$1" ]; then
         vim -p `git diff-tree --no-commit-id --name-only -r HEAD | paste -s -d ' '`
     else
         vim -p `git diff-tree --no-commit-id --name-only -r $1 | paste -s -d ' '`
     fi
+}
 
 function jshiglight() {
   highlight -O rtf -S js -s molokai -j 3 -K 24 -n $1 | pbcopy
+}
+
+function f() {
+  find . -iname "*$1*" ${@:2}
+}
+
+function r() {
+  grep "$1" ${@:2} -R .
+}
+
+#mkdir and cd
+function mkcd() {
+  mkdir -p "$@" && cd "$_";
 }
